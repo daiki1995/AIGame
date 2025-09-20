996026696da8b177028d143ab3048ad949c59e30
法師人大基 <>
2025-09-20 21:27:35 +0900

    第一プッシュ

---
+++ script.js
@@ -0,0 +1,43 @@
+let urls = [
+    {
+        title: 'Google',
+        url: 'https://www.google.com',
+        description: '世界最大の検索エンジン'
+    },
+    {
+        title: 'YouTube',
+        url: 'https://www.youtube.com',
+        description: '動画共有プラットフォーム'
+    },
+    {
+        title: 'GitHub',
+        url: 'https://github.com',
+        description: 'コード管理とコラボレーションのプラットフォーム'
+    }
+];
+
+function deleteUrl(index) {
+    urls.splice(index, 1);
+    displayUrls();
+}
+
+function displayUrls() {
+    const listElement = document.getElementById('urlList');
+    
+    if (urls.length === 0) {
+        listElement.innerHTML = '<div class="empty">URLが登録されていません</div>';
+        return;
+    }
+    
+    listElement.innerHTML = urls.map((item, index) => `
+        <div class="url-item">
+            <div class="url-title">${item.title}</div>
+            <a href="${item.url}" target="_blank" class="url-link">${item.url}</a>
+            <div class="url-description">${item.description}</div>
+            <button class="delete-btn" onclick="deleteUrl(${index})">削除</button>
+        </div>
+    `).join('');
+}
+
+// ページ読み込み時にURLを表示
+displayUrls();
